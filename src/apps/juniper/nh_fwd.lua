33b7cccb5e05fccb8391cf6b43b357e2cad1e65f
Marcel Wiget <mwiget@juniper.net>
2016-01-13 09:52:41 +0100

    nh caching now via duplicated packet and special source ip

--- src/apps/juniper/nh_fwd.lua
+++ src/apps/juniper/nh_fwd.lua
@@ -10,11 +10,67 @@ local ipv6 = require("lib.protocol.ipv6")
 
 local ffi = require("ffi")
 local C = ffi.C
+local cast = ffi.cast
 
+local uint16_ptr_t = ffi.typeof("uint16_t*")
+
+function wr16(offset, val)
+  cast(uint16_ptr_t, offset)[0] = val
+end
 
 --- # `nh_fwd` app: Finds next hop mac by sending packets to VM interface
 
 nh_fwd = {}
+
+local function hex_dump(cdata,len)
+  local buf = ffi.string(cdata,len)
+  for i=1,math.ceil(#buf/16) * 16 do
+    if (i-1) % 16 == 0 then io.write(string.format('%08X  ', i-1)) end
+    io.write( i > #buf and '   ' or string.format('%02X ', buf:byte(i)) )
+    if i %  8 == 0 then io.write(' ') end
+    if i % 16 == 0 then io.write( buf:sub(i-16+1, i):gsub('%c','.'), '\n' ) end
+  end
+end
+
+function send_cache_trigger(r, p, mac)
+
+-- set a bogus source IP address of 0.0.0.0 or fe80::, so we can recognize it
+-- later when it comes back from the vMX.
+-- TODO: tried initially to use ::0 as source, but such packets are discarded
+-- by the vmx due to RFC 4007, chapter 9, which also considers the source IPv6
+-- address.
+-- Using the link local address fe80::, the packets are properly routed back
+-- thru the same interface. Not sure if its ok to use that address or if there
+-- is a better way.
+
+  local ETH_HDR_SIZE = 14
+  local IPV4_HDR_SIZE  = 20
+  local IPV6_HDR_SIZE  = 40
+
+  local eth_header = ethernet:new_from_mem(p.data, ETH_HDR_SIZE)
+  local ether_type = eth_header:type()
+  if 0x0800 == ether_type and p.length > ETH_HDR_SIZE + IPV4_HDR_SIZE then
+    -- IPv4 packet
+    local ipv4_header = ipv4:new_from_mem(p.data + ETH_HDR_SIZE, IPV4_HDR_SIZE) 
+    ipv4_header:src(ipv4:pton('0.0.0.0'))
+    wr16(p.data + 24, 0)  -- clear checksum before calculation
+    ipv4_header:checksum()
+  elseif 0x86dd == ether_type and p.length > ETH_HDR_SIZE + IPV6_HDR_SIZE then
+    -- IPv6 packet
+    local ipv6_header = ipv6:new_from_mem(p.data + ETH_HDR_SIZE, IPV6_HDR_SIZE) 
+    ipv6_header:src(ipv6:pton('fe80::'))
+  else
+    -- dont know what to do. Drop it silently
+    r = nil 
+  end
+  eth_header:dst(mac)
+  if r and not link.full(r) then
+    link.transmit(r, p)
+  else
+    packet.free(p)
+  end
+
+end
 
 function nh_fwd:new(arg)
   local conf = arg and config.parse_app_arg(arg) or {}
@@ -110,14 +166,13 @@ function nh_fwd:push ()
       local current_time = tonumber(app.now())
       if current_time > self.cache_refresh_time + self.cache_refresh_interval then
         self.cache_refresh_time = current_time
-        eth_header:dst(self.mac_address)
-        output = self.output.vmx
-      else
-        -- set nh mac and send the packet out the wire 
-        eth_header:dst(self.next_hop_mac)
+        send_cache_trigger(self.output.vmx, packet.clone(p), self.mac_address)
       end
+      -- set nh mac and send the packet out the wire 
+      eth_header:dst(self.next_hop_mac)
     else
-      -- need to resolve nh. Punch it to vmx
+      -- need to resolve nh. Punch plus a cache trigger to vmx
+      send_cache_trigger(self.output.vmx, packet.clone(p), self.mac_address)
       eth_header:dst(self.mac_address)
       output = self.output.vmx
     end
@@ -147,14 +202,15 @@ function nh_fwd:push ()
           local learn = nil
           if 0x86dd == ether_type then
             local ipv6_header = ipv6:new_from_mem(p.data + ETH_HDR_SIZE, IPV6_HDR_SIZE) 
-            -- check for IPIP next header. Only use those packets for
-            -- nh learning
-            if 0x04 == ipv6_header:next_header() then
+            if ipv6_header:src_eq(ipv6:pton('fe80::')) and 0x04 == ipv6_header:next_header() then
               learn = "ipv6"
             end
           end
           if 0x0800 == ether_type then
-            learn = "ipv4"
+            local ipv4_header = ipv4:new_from_mem(p.data + ETH_HDR_SIZE, IPV4_HDR_SIZE) 
+            if ipv4_header:src_eq(ipv4:pton('0.0.0.0')) then
+              learn = "ipv4"
+            end
           end
           if learn then
             local mac = eth_header:dst()
@@ -162,6 +218,9 @@ function nh_fwd:push ()
               self.next_hop_mac = ethernet:pton("00:00:00:00:00:00")
               ffi.copy(self.next_hop_mac, eth_header:dst(), 6)
               print("learning " .. learn .. " nh mac address " .. ethernet:ntop(self.next_hop_mac))
+              -- make sure we free this packe without sending it, this was
+              -- a self created packet to learn the nh only
+              output = nil 
             end
           end
         end
