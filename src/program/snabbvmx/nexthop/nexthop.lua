c615d5a6d7f1123b9b4bbdf09055a5b3dc33207c
Marcel Wiget <mwiget@juniper.net>
2016-05-08 15:53:40 +0200

    ignore snabb processes that don't have nexthop (like packetblaster)

--- src/program/snabbvmx/nexthop/nexthop.lua
+++ src/program/snabbvmx/nexthop/nexthop.lua
@@ -5,11 +5,18 @@ local lib = require("core.lib")
 local shm = require("core.shm")
 local syscall = require("syscall")
 local ethernet = require("lib.protocol.ethernet")
+local S          = require("syscall")
 local usage = require("program.snabbvmx.nexthop.README_inc")
 
 local long_opts = {
    help = "h"
 }
+
+local function file_exists(path)
+   local stat = S.stat(path)
+   return stat and stat.isreg
+end
+
 
 function run (args)
 
@@ -25,16 +32,18 @@ function run (args)
       else
 --        print("pid " .. pid .. " type is " .. type(pid))
         local nh_v4_path = "//" .. pid .. "/next_hop_mac_v4"
+        if file_exists("/var/run/snabb" .. nh_v4_path) then
+           local nh_v4 = shm.open(nh_v4_path, "struct { uint8_t ether[6]; }", true)
+           print(string.format("%d: next_hop_mac for IPv4 is %s", pid, ethernet:ntop(nh_v4.ether)))
+           shm.unmap(nh_v4)
+        end
         local nh_v6_path = "//" .. pid .. "/next_hop_mac_v6"
-        local nh_v4 = shm.open(nh_v4_path, "struct { uint8_t ether[6]; }")
-        local nh_v6 = shm.open(nh_v6_path, "struct { uint8_t ether[6]; }")
-
-        print(string.format("%d: next_hop_mac for IPv4 is %s", pid, ethernet:ntop(nh_v4.ether)))
-        print(string.format("%d: next_hop_mac for IPv6 is %s", pid, ethernet:ntop(nh_v6.ether)))
-
-        shm.unmap(nh_v4)
-        shm.unmap(nh_v6)
-      end
+        if file_exists("/var/run/snabb" .. nh_v6_path) then
+           local nh_v6 = shm.open(nh_v6_path, "struct { uint8_t ether[6]; }", true)
+           print(string.format("%d: next_hop_mac for IPv6 is %s", pid, ethernet:ntop(nh_v6.ether)))
+           shm.unmap(nh_v6)
+        end
+     end
     end
   end
 
